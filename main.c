a0bc32e5645f080e1f1a54c19bf47108edb025ef
edwinmanuelcabrerafrias072007-web <edwinmanuelcabrerafrias072007@gmail.com>
2026-03-28 22:18:58 -0400

    Intento de creacion de juego de cine sencilla

--- main.c
+++ main.c
@@ -1,104 +1,324 @@
+/*
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX 100
+
+void contar(char *c);
+void palabra_mas_grande(char *c);
+
+int main() {
+
+    char *c = calloc(MAX,sizeof(char));
+
+    printf("Frase ==> ");
+    fgets(c,MAX,stdin);
+
+    printf("%s",c);
+
+    contar(c);
+    palabra_mas_grande(c);
 
 
-     struct trabajador {
 
-        int id;
-        char nombre[50];
-        char nacionalidad[30];
-        char num_telefono[40];
-        char sector_vivienda[50];
-     };
-
-//Funciones//
-
-void llenar_regi(struct trabajador *p);
-void ver_registro(struct trabajador *p);
-
-int main(){
-
-    struct trabajador e1[4];
-    struct trabajador *p = &e1[0];
-    llenar_regi(e1);
-    ver_registro(e1);
 
     return 0;
 }
 
-void llenar_regi(struct trabajador *p){
 
-    char conditional;
-    char conditional2;
-    int i =0 ;
+void contar(char *c){ //longitud de la palabra //
 
-    printf("==== Formulario de registro de trabajador ==== \n\n");
+    int cantidad;
 
-    do{
+    cantidad = strlen(c);
 
-        printf("[Registro (%d)] \n",i+1);
-
-        printf("ID ==> ");
-        scanf("%d",&(p+i)->id);
-
-        while(getchar() != '\n');
-
-        printf("Nombre ==> ");
-        gets((p+i)->nombre);
-
-        printf("Nacionalidad ==> ");
-        gets((p+i)->nacionalidad);
-
-        printf("Numero de Telefono ==> ");
-        gets((p+i)->num_telefono);
-
-        printf("Area Desempeño ==> ");
-        gets((p+i)->sector_vivienda);
-
-        printf("Desea continuar ingresando S/N ==> ");
-        scanf(" %c",&conditional);
-
-        i++;
-
-        system("cls");
-
-        if(i == 4 ){
-
-            printf("\n\tLIMITE DE REGISTROS ADMINITDOS\n");
-
-            printf("DESEA VER LOS REGISTROS HECHOS S/N ==> ");
-            scanf(" %c",&conditional2);
-
-            if(conditional2 == 's'){
-                ver_registro(e1);
-            }
-        }
-
-
-
-    }while(conditional == 's' && i < 4);
-
-
-
-
+    printf("\n\nTamano de la cadena ==> %d",cantidad);
 }
 
 
-void ver_registro(struct trabajador *p){
+
+void palabra_mas_grande(char *c){
+
+    int max = 0;
+    int actual = 0;
+
+    char palabra[MAX];
+    char mayor[MAX];
+
+    int j = 0 ;
+
+    for(int i = 0 ; c[i] != '\0' ; i++ ){
+
+        if(c[i] != ' ' && c[i] != '\n'){
+            palabra[j] = c[i];
+            j++;
+            actual ++ ;
+        }else {
+            palabra[j] = '\0 ';
+
+            if(actual > max){
+                max = actual;
+                strcpy(mayor , palabra);
+            }
+
+            actual = 0 ;
+            j = 0 ;
+        }
+    }
+
+    palabra[j] = '\0';
+    if(actual > max ){
+    strcpy(mayor,palabra);
+
+    }
+    printf("\n\nLa palabra mas grande es %s",mayor);
+}
+*/
+
+
+
+
+
+
+/*
+
+//ejercicios //
+
+// 1- crear una funcion que retorne un valor y diga si es par   LISTO//
+// 2- crear una funcion que reciba un numero y retorne el cubo  LISTO//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Funciones //
+
+int par(int a);
+int cubo(int a);
+
+int main () {
+
+    int a;
+
+
+    printf("Digite la cantidad  == > ");
+    scanf("%d",&a);
+
+    int numero = par(a);
+
+    if (numero == 0 ){
+
+        printf("EL NUMERO ES IMPAR");
+    }else{
+        printf("EL NUMERO ES PAR");
+    }
+
+    int resultado2 = cubo(a);
+
+    printf("\nEl cubo de ese numero es [%d]",resultado2);
+
+
+
+    return 0 ;
+}
+
+
+int par(int a){
+    if(a % 2 == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int cubo(int a){
+
+    int resultado;
+
+    resultado = a * a * a ;
+
+    return resultado;
+}
+*/
+
+
+/*
+// Ejercicio 2 Listo //
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Funciones //
+
+int mayor (int a , int b );
+
+int main () {
+
+    int a,b ;
+
+    printf("Digite numero 1 ==> ");
+    scanf("%d",&a);
+
+    printf("Digite numero 2 ==> ");
+    scanf("%d",&b);
+
+    int resultado = mayor(a,b);
+
+    printf("mayor fue ==> %d",resultado);
+
+    return 0 ;
+}
+
+int mayor (int a , int b ){
+
+    int mayor;
+
+    if(a > b ){
+
+        return a ;
+    }else {
+        return b;
+    }
+
+}
+
+*/
+
+
+/*
+//Ejemplo de signo tenario para los if  ///
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+
+    int edad ;
+
+    printf("Digite su eda ==> ");
+    scanf("%d",&edad);
+
+
+    char *mensaje = (edad >= 18) ? "El pana es mayor " : "El pana es menor" ;
+
+    printf("%s",mensaje);
+
+
+    return 0;
+ }
+
+
+ */
+
+
+/*
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ //funciones //
+
+ int comp(int a);
+
+ int main() {
+
+    int a ;
+
+    printf("Digite el numero => ");
+    scanf("%d",&a);
+
+    int resultado = comp(a);
+
+    char *frase = (resultado == 1) ? "Es positivo" : (resultado == 0 ? "Es cero" : "Es negativo");
+
+    printf("%s",frase);
+
+
+    return 0;
+ }
+
+
+  int comp(int a){
+
+    return (a > 0 ) ? 1 : (a == 0 ? 0 : -1);
+
+  }
+
+
+  */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+//Funciones//
+
+void space(){printf("\n\n");}
+void llenar_matriz(int x, int y , int matriz [x][y]);
+void imprimir_matriz(int x , int y , int matriz[x][y]);
+
+
+int main () {
+
+    int x,y;
+    srand(time(NULL));
+
+    printf("Dimenciones de la matriz [X]==> ");
+    scanf("%d",&x);
+
+    printf("Dimenciones de la matriz [Y] ==> ");
+    scanf("%d",&y);
+
+
+    printf("==== DIMENCIONES ESTABLECIDAS [%d] X [%d] PARA LA MATRIZ ====",x,y);
+
+    space();
+
+    int matriz[x][y];
+
+    llenar_matriz(x,y,matriz);
 
     system("cls");
 
-    printf("==== FORMULARIO DE REGISTROS RECIENTES ====");
+    imprimir_matriz(x,y,matriz);
 
-    for(int i = 0 ; i < 4 ; i++){
+    return 0;
+}
 
-        printf("[Registro (%d)]",i+1);
-        printf("ID => %d ",(p+i)->id);
-        printf("Nombre => %s",(p+i)->nombre);
-        printf("Nacionalidad => %s",(p+i)->nacionalidad);
-        printf("Numero de Telefono => %s",(p+i)->num_telefono);
-        printf("Sector de Vivienda => %s",(p+i)->sector_vivienda);
+void llenar_matriz(int x , int y , int matriz[x][y] ){
 
+    for(int i = 0 ; i < x ; i++){
+        for(int j = 0 ; j < y ; j++){
+            matriz[i][j] = rand() % 9 ;
+        }
+    }
+}
+
+
+void imprimir_matriz(int x , int y , int matriz[x][y]){
+
+    printf(" ==== Tablero inicial de cine ==== \n\n");
+
+
+    for(int i = 0 ; i < x ; i++){
+        for(int j = 0 ; j < y ; j++){
+            printf("\t%d",matriz[i][j]);
+
+        }
+        printf("\n");
     }
 
 }
